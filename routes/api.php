a6716c4942c45ddadcfaddbda321b74a46db3471
blueorbitz <virtue.sign@gmail.com>
2023-11-27 16:46:55 +0000

    Build: (f9cc14e) Setup webhost with laravel & client configuration

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\CheckSessionController;
+use App\Http\Controllers\EditUserController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/', function(Request $request) {
+    return 'hi root';
+});
+
+Route::get('/greeting', function(Request $request) {
+    return 'hi greeting';
+});
+
+Route::post('/users/register', RegisterController::class)->name('register');
+Route::post('/users/login', LoginController::class)->name('login');
+Route::post('/users/logout', LogoutController::class)->name('logout');
+Route::post('/users/checkSession', CheckSessionController::class)->name('check-session');
+Route::any('/users/edit', EditUserController::class)->name('edit-user');
+
+Route::middleware(['auth:api'])->group(function () {
+    //
+});
